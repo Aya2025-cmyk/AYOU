37fc95996f5ce6dd94c244bac46190e5c662296f
Aya2025-cmyk <ayambathieu8@gmail.com>
2025-02-14 00:02:40 +0000

    Add files via upload

--- APPLICATION.py
+++ APPLICATION.py
@@ -43,7 +43,7 @@ def add_bg_from_local(image_file):
     }}
     
     .stButton>button {{
-        background-color:#5D4037; 
+        background-color:#000000; 
         border-radius: 10px;
         border: none;
         padding: 8px 15px;
